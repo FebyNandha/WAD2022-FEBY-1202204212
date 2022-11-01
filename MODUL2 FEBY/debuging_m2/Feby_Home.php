47b5bd13c2db65da95222d50bcb737205c967612
alaric2001 <alaric2001ra@gmail.com>
2022-11-01 07:26:25 +0700

    debuging m2

---
+++ MODUL2 FEBY/debuging_m2/Feby_Home.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MODUL2 FEBY</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="Feby_CSS.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="Feby_Home.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="Booking_Feby.php">Booking</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="home">
+        <div class="kolom" align="center">
+          <h4>WELCOME TO EAD RENT</h4>
+          <p>Find your best deal, here!</p>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+      <div class="col">
+        <div class="card">
+            <img src="rush.png" class="card-img-top" alt="Toyota Rush">
+            <div class="card-body">
+                <h5 class="card-title">Toyota Rush</h5>
+                <p class="card-text">Rp. 200000 / Day</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item text-primary">7 Kursi</li>
+                <li class="list-group-item text-primary">1.500 CC</li>
+                <li class="list-group-item text-primary">Manual</li>
+            </ul>
+            <div class="cardbody bg-light">
+                <a href="Feby_Booking.php" class="btn btn-primary">Book Now</a>
+            </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+            <img src="ayla.jpg" class="card-img-top" alt="Toyota Ayla">
+            <div class="card-body">
+                <h5 class="card-title">Toyota Ayla</h5>
+                <p class="card-text">Rp. 150000 / Day</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item text-primary">5 Kursi</li>
+                <li class="list-group-item text-primary">1.200 CC</li>
+                <li class="list-group-item text-primary">Manual</li>
+            </ul>
+            <div class="cardbody bg-light">
+                <a href="Feby_Booking.php" class="btn btn-primary">Book Now</a>
+            </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+            <img src="brio.jpg" class="card-img-top" alt="Honda Brio" height="165">
+            <div class="card-body">
+                <h5 class="card-title">Honda Brio</h5>
+                <p class="card-text">Rp. 150000 / Day</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item text-primary">5 Kursi</li>
+                <li class="list-group-item text-primary">1.200 CC</li>
+                <li class="list-group-item text-primary">Automatic</li>
+            </ul>
+            <div class="cardbody bg-light">
+                <a href="Feby_Booking.php" class="btn btn-primary">Book Now</a>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+        <p>Created by Feby_1202204212</p>
+    </div>
+
+  </body>
+</html>
